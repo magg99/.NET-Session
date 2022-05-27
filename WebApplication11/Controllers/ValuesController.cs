b1fa5f09ca55b9ed2549829b83cc0efcf1db2c56
magg99 <mayank.aggarwal@cygrp.com>
2022-05-27 12:29:34 +0530

    Commit by Mayank

---
+++ WebApplication11/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTEx.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet,Authorize]
+        public IEnumerable<Book> Get()
+        {
+            var currentUser = HttpContext.User;
+            var resultBookList = new Book[] {
+        new Book { Author = "Ray Bradbury",Title = "Fahrenheit 451" },
+        new Book { Author = "Gabriel García Márquez", Title = "One Hundred years of Solitude" },
+        new Book { Author = "George Orwell", Title = "1984" },
+        new Book { Author = "Anais Nin", Title = "Delta of Venus" }
+      };
+
+            return resultBookList;
+        }
+        public class Book
+        {
+            public string Author { get; set; }
+            public string Title { get; set; }
+            public bool AgeRestriction { get; set; }
+        }
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
